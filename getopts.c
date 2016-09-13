928e10e0f95eb396bc89f7a5ff494dae425d594f
Sarah Botwinick <scb219@chiron.cse.lehigh.edu>
2016-09-12 23:12:08 -0400

    inittial commit

---
+++ getopts.c
@@ -0,0 +1,7 @@
+/*
+  CSE 109
+  Sarah Botwinick
+  scb219
+  Program Description:getopts file for my shell
+  Program #2
+*/
---
+++ prog2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/*
+  CSE 109
+  Sarah Botwinick
+  scb219
+  Program Description:Main function for a shell
+  Program #2
+*/
