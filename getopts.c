734604e38d5f4340a12f735d7b9239ff3ff9238a
Sarah Botwinick <sbotwinick@172-18-33-24.DYNAPOOL.NYU.EDU>
2017-07-18 23:01:51 -0400

    added simple shell to github

--- getopts.c
+++ getopts.c
@@ -1,18 +1,13 @@
 /*
-  CSE 109
   Sarah Botwinick
   scb219
   Program Description:getopts file for my shell
-  Program #2
 */
 #include <string.h>
 #include <stdlib.h>
 
 char ** getopts(char cmd[]){
 
- 
-  //char ncmd[sizeof(cmd)];
-//strcpy(ncmd, cmd);
   //split into tokens
 char *token = strtok(cmd, " ");
 //create a counter variable
@@ -21,7 +16,7 @@ char *token = strtok(cmd, " ");
    place++;
    token = strtok(NULL, " ");
  }
-//int x = sizeof(token);
+
 //create array for tokens
  char** tokarray;
  tokarray= malloc((sizeof (char*))*place);
@@ -29,7 +24,7 @@ char *token = strtok(cmd, " ");
  int x = 0;
 
  while(token !=NULL){
-   //   int stringlength = strlen(place);
+
    tokarray[x] = malloc(place + 1);
    //add tokens to array
    strcpy(tokarray[x], token);
--- prog2.c
+++ prog2.c
@@ -4,17 +4,14 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 /*
-  CSE 109
   Sarah Botwinick
   scb219
   Program Description:Main function for a shell
-  Program #2
 */
 char ** getopts(char cmd[]);
 
 
 int main(int argc, char *argv[]){
-  // char hostcomp[] = gethostname();
   char name[100];
   gethostname(name,100);
   strcat(name,">");
@@ -25,21 +22,16 @@ int main(int argc, char *argv[]){
   while(y == 3){
   char comstr[100];
   fgets(comstr, sizeof(comstr), stdin);
-  // printf(comstr);
 
    //chop off end line character
 
-   //  comstr[strlen(comstr)-1] = 0;
-   // char *instr = malloc(strlen(comstr)+1);
-   //  strcpy(instr, comstr);
+
    comstr[strlen(comstr) - 1] = '\0';
-   //   printf(comstr);
+
 
    //check if command is exit
    int x = strcmp(comstr, "exit");
    if(x == 0){
-     // printf("exited");
-     // y = 7;
      return 0;
    }
    else{
